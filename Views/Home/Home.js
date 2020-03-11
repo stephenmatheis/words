c9161218f670530c57387e346ebe2328101bc060
Stephen Matheis <stephen.a.matheis@gmail.com>
2020-03-11 07:20:17 -0500

    remove PII

--- Views/Home/Home.js
+++ Views/Home/Home.js
@@ -4,13 +4,8 @@ import Component_Card from '../../Components/Card.js';
 
 export default function View_Home(param) {
     const card = Component_Card({
-        title: 'Stephen Matheis',
+        title: 'Card',
         innerContent: /*html*/ `
-            <p>Software Developer</p>
-            <p>San Antonio, TX</p>
-            <p>Washington, D.C</p>
-            <p>(912) 492-2522</p>
-            <p>stephen.a.matheis@gmail.com</p>
         `,
         adjacentElement: app.store.getMainContainer()
     });
