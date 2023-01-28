7805483e9fae02c6f0db3aae2099fb133bf34731
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-01-28 02:13:12 -0600

    Remove unused font loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,7 @@
 import Head from 'next/head'
 
-import { Space_Mono } from '@next/font/google'
-
 import '@/styles/globals.scss'
 import '@/styles/resume.scss'
-
-const spaceMono = Space_Mono({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-});
 
 export default function App({ Component, pageProps }) {
     return (
@@ -17,9 +10,7 @@ export default function App({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
             </Head>
-            {/* <main className={spaceMono.className}> */}
             <Component {...pageProps} />
-            {/* </main> */}
         </>
     )
 }
