97f6815a436863c92915ec851cc8155afd4a9c1a
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-01-28 00:40:33 -0600

    Reset resume styles (WIP)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
+import Head from 'next/head'
+
+import { Space_Mono } from '@next/font/google'
+
 import '@/styles/globals.scss'
 import '@/styles/resume.scss'
 
+const spaceMono = Space_Mono({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+            </Head>
+            {/* <main className={spaceMono.className}> */}
+            <Component {...pageProps} />
+            {/* </main> */}
+        </>
+    )
 }
