01f0537d17b3034032b104a9633cd6973b510d29
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-02-05 20:21:20 -0600

    Create gui.js

---
+++ pages/gui.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { } from 'react';
+
+export default function Gui() {
+    return (
+        <>
+            <Head>
+                <title>Stephen Matheis | GUI</title>
+                <meta name="description" content="Stephen Matheis" />
+            </Head>
+            <div>
+
+            </div>
+        </>
+    )
+}
