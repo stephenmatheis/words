efd0ed284367fbdd56cd4ff2b2f9118b3eea08d7
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-02-23 21:10:52 -0600

    Remove blinking animation

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -6,7 +6,6 @@ import styles from './header.module.scss';
 
 export default function Header({ loading, setLoading, speed, showLinkBackground }) {
     // State
-    const [isBlinking, setIsBlinking] = useState(false); // OFF
     const [transitionBackgroundColor, setTransitionBackgroundColor] = useState(false);
 
     // Modifiers
@@ -38,16 +37,6 @@ export default function Header({ loading, setLoading, speed, showLinkBackground 
     const toEngineer = useRef();
 
     useEffect(() => {
-        setTimeout(() => {
-            setIsBlinking(false);
-        }, 1000 * 3);
-    }, [isBlinking]);
-
-    useEffect(() => {
-        if (isBlinking) {
-            return;
-        }
-
         setTimeout(async () => {
             if (!overlay.current || !ctr.current) {
                 return;
@@ -114,7 +103,7 @@ export default function Header({ loading, setLoading, speed, showLinkBackground 
                 return 'done';
             }
         }
-    }, [isBlinking, loading, setLoading, speed]);
+    }, [loading, setLoading, speed]);
 
     return (
         <header className={styles['header']}>
@@ -137,31 +126,14 @@ export default function Header({ loading, setLoading, speed, showLinkBackground 
                 loading &&
                 <div ref={overlay} className={classNames(styles['loading-overlay'], { [styles['background-color']]: transitionBackgroundColor })}>
                     <div ref={ctr} className={styles['ctr']}>
-                        {
-                            isBlinking ?
-                                <>
-                                    <span className={classNames(styles['name'], styles['blink'])}>
-                                        |
-                                        <span className={styles['hidden']}>
-                                            {firstName.substring(1)}
-                                        </span>
-                                    </span>
-                                    <span className={classNames(styles['hidden'], styles['name'])}>{lastName}</span>
-                                    <span className={classNames(styles['hidden'], styles['title'])}>{frontEnd}</span>
-                                    <span className={classNames(styles['hidden'], styles['title'])}>{software}</span>
-                                    <span className={classNames(styles['hidden'], styles['title'])}>{engineer}</span>
-                                </> :
-                                <>
-                                    {/* Name */}
-                                    <Type ref={fromFirstName} content={firstName} speed={speed * 2 / 3} className={styles['name']} wrapperClass={styles['line-wrapper']} />
-                                    <Type ref={fromLastName} content={lastName} speed={speed * 2 / 3} delay={delayAfter(firstName, speed)} className={styles['name']} wrapperClass={styles['line-wrapper']} />
+                        {/* Name */}
+                        <Type ref={fromFirstName} content={firstName} speed={speed * 2 / 3} className={styles['name']} wrapperClass={styles['line-wrapper']} />
+                        <Type ref={fromLastName} content={lastName} speed={speed * 2 / 3} delay={delayAfter(firstName, speed)} className={styles['name']} wrapperClass={styles['line-wrapper']} />
 
-                                    {/* Title */}
-                                    <Type ref={fromFrontEnd} content={frontEnd} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
-                                    <Type ref={fromSoftware} content={software} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName + frontEnd, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
-                                    <Type ref={fromEngineer} content={engineer} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName + frontEnd + software, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
-                                </>
-                        }
+                        {/* Title */}
+                        <Type ref={fromFrontEnd} content={frontEnd} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
+                        <Type ref={fromSoftware} content={software} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName + frontEnd, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
+                        <Type ref={fromEngineer} content={engineer} speed={speed * 2 / 3} delay={delayAfter(firstName + lastName + frontEnd + software, speed, titleMod)} className={styles['title']} wrapperClass={styles['line-wrapper']} />
                     </div>
                 </div>
             }
